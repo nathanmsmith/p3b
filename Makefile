c206c797786395e9697f887a6434a21ecae66847
Nathan Smith <nathan@nathansmith.io>
2017-11-26 22:34:19 -0800

    Initial commit

---
+++ Makefile
@@ -0,0 +1,15 @@
+# NAME: Dawei Huang, Nathan Smith 
+# EMAIL: daweihuang@ucla.edu, nathan.smith@ucla.edu
+# ID: 304792166, 704787554
+
+default: make
+
+make:
+	cp lab3b.py lab3b
+	chmod +x lab3b lab3b.py
+
+clean:
+	rm -f lab3b lab3b-304792166.tar.gz
+
+dist: clean
+	tar -zcvf lab3b-304792166.tar.gz Makefile README lab3a.py
---
+++ lab3b.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# NAME: Dawei Huang, Nathan Smith
+# EMAIL: daweihuang@ucla.edu, nathan.smith@ucla.edu
+# ID: 304792166, 704787554
+
+import sys
+import os
+
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv[1:]) != 1:
+        print("[Error]: Incorrect number of arguments.", file=sys.stderr)
+        sys.exit(1)
+
+    if not os.path.isfile(sys.argv[1]):
+        print("[Error]: File does not exist.", file=sys.stderr)
+        sys.exit(1)
+
+    # Block Consistency Audits
+    # I-node Allocation Audits
+    # Directory Consistency Audits
